770abdd2f600f9b6379d94eb42a89e5b22c7fa97
BulinskiD <dawidbulinski132@gmail.com>
2019-06-07 14:12:03 +0200

    Removed placeholder actions and reducers

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-import {TEST_ACTION} from '../constants/actionTypes';
-
-export const testAction = () => {
-    return {
-        type: TEST_ACTION
-    }
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,7 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import { TEST_ACTION } from "../constants/actionTypes";
-
-const testReducer = (test = "sth", action) => {
-    if(action.type === TEST_ACTION) {
-        return "I WAS INVOKED!";
-    }
-    return test;
-}
 
 export default combineReducers(
     {
-                test: testReducer,
                 user: authReducer
             });
