a9ee64cdc51484f313496f1d2c7c6e8730ed1660
BulinskiD <dawidbulinski132@gmail.com>
2019-06-17 11:00:39 +0200

    Unit test for seedCheckers method

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
+import "core-js/stable";
 
 configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
